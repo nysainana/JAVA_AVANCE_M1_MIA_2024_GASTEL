9e1a39bd948e898ea493cb7f3bfef48c22de113f
Ny Sainana <manjaka.nyshainana@gmail.com>
2024-06-19 21:10:52 +0300

    refactor: Add resizable pane, ajout en tant que canvas

--- src/main/java/com/java/avance/javaavance2024/ui/App.java
+++ src/main/java/com/java/avance/javaavance2024/ui/App.java
@@ -1,12 +1,28 @@
 package com.java.avance.javaavance2024.ui;
 
 
+import com.java.avance.javaavance2024.components.ResizablePane;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.layout.StackPane;
 
-public class App {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class App implements Initializable {
 
     @FXML
+    private StackPane paneCanvas;
+    @FXML
     public StackPane root;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ResizablePane pane = new ResizablePane();
+        pane.setPrefWidth(500);
+        pane.setPrefHeight(500);
+
+        paneCanvas.getChildren().add(pane);
+    }
 
 }
